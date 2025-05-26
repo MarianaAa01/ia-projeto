163b9ed9c2bf3ba0c65bf31fc6f717795c40f834
Mariana Carvalho <mariana.d.carvalho@tecnico.ulisboa.pt>
2025-05-26 20:35:53 +0100

    Primeira parte do projeto

--- proj2425base-nuruomino/nuruomino.py
+++ proj2425base-nuruomino/nuruomino.py
@@ -9,6 +9,7 @@
 class NuruominoState:
     state_id = 0
 
+    #guarda o tabuleiro (APAGAR COMENTÁRIO)
     def __init__(self, board):
         self.board = board
         self.id = Nuroumino.state_id
@@ -19,23 +20,56 @@ class NuruominoState:
         de abertos nas procuras informadas. """
         return self.id < other.id
 
+
 class Board:
     """Representação interna de um tabuleiro do Puzzle Nuruomino."""
+
+    #define o tabuleiro 
+    def __init__(self, grid):
+        self.grid = grid
 
     def adjacent_regions(self, region:int) -> list:
         """Devolve uma lista das regiões que fazem fronteira com a região enviada no argumento."""
         #TODO
-        pass
+        #Criar uma lista vazia
+        adjacents = set()
+        #Percorrer a matriz dada
+        #Quando encontrar o inteiro nessa matriz tenho de encontrar os seus adjacentes (fazer com o adjacent_values)
+        for row in range(len(self.grid)):
+            for col in range(len(self.grid[0])):
+                #encontrei o inteiro nessa matriz
+                if self.grid[row][col] == region:
+                    #percorrer as posições adjacentes que eu calcular
+                    for r, c in self.adjacent_positions(row, col):
+                        #temporário
+                        val = self.grid[r][c]
+                        #comparar se a adjacente é diferente do inteiro
+                        if val != region:
+                            adjacents.add(val)
+        return list(adjacents)
     
     def adjacent_positions(self, row:int, col:int) -> list:
         """Devolve as posições adjacentes à região, em todas as direções, incluindo diagonais."""
         #TODO
-        pass
+        #Lista vazia onde vou colocar os adjacentes da posição recebida (APAGAR COMENTÁRIO)
+        positions = []
+        #estes deslocamentos vão obter as 8 adjacentes (4 diagonais e 4 em cruz/diretas whatever)
+        for dr in [-1, 0, 1]: #delta row
+            for dc in [-1, 0, 1]: #delta column
+                if dr == 0 and dc == 0: #não mudou nada
+                    continue
+                r, c = row + dr, col + dc #calculo a "nova posição" a analisar com base no desvio criado nos fors
+                #testar os limites da matriz
+                if 0 <= r < len(self.grid) and 0 <= c < len(self.grid[0]):
+                    positions.append((r, c))
+        return positions
 
     def adjacent_values(self, row:int, col:int) -> list:
         """Devolve os valores das celulas adjacentes à região, em todas as direções, incluindo diagonais."""
         #TODO
-        pass
+        #percorre todas as posições da grid (row, col) e devolve os pares (r,c)
+        #para cada (r,c) (posição dum vizinho, faz o self.grid[r][c] nessa posição)
+        return [self.grid[r][c] for r, c in self.adjacent_positions(row, col)]
     
     
     @staticmethod
@@ -49,8 +83,19 @@ class Board:
             > from sys import stdin
             > line = stdin.readline().split()
         """
-        #TODO
-        pass    
+        from sys import stdin
+
+        grid = []
+        for line in stdin:
+            #Ignora linhas vazias
+            if line.strip() == "":
+                continue
+            #Converte a linha em inteiros
+            row = list(map(int, line.strip().split()))
+            grid.append(row)
+
+        return Board(grid)   
+
 
     # TODO: outros metodos da classe Board
 
